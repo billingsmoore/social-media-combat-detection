2844273de95240a970e00dc4ae1f5de1fd4d8e1e
Jacob Moore <billingsmoore@gmail.com>
2023-03-06 21:14:08 -0500

    rearranged code into functions for modularity

--- process_html.py
+++ process_html.py
@@ -1,38 +1,65 @@
 from bs4 import BeautifulSoup
 
-# import html
-
-file = open('sample.html', 'r')
-
-contents = file.read()
-
-soup = BeautifulSoup(contents, 'lxml')
-
-text = soup.text
-
-split_text = text.split()
-
-# retrieve messages from html
-
-messages = []
-
-for i in range(len(split_text)):
-    if split_text[i] == 'TRANSLATION':
-        message = []
-        message.append(split_text[i])
-        j = 1
-        while split_text[i + j] != '/>ORIGINAL':
-            message.append(split_text[i + j])
-            j += 1
-        messages.append(message)
 
 
-# clean messages
-removal_list = ['/>', '<br', 'TRANSLATION', ':', 'SUBSCRIBE']
+# helper function to retrieve messages from html
 
-for message in messages:
-    for word in message:
-        if word in removal_list:
-            message.remove(word)
+def retrieve_messages(split_text):
+
+    messages = []
+
+    for i in range(len(split_text)):
+        if split_text[i] == 'TRANSLATION':
+            message = []
+            message.append(split_text[i])
+            j = 1
+            while split_text[i + j] != '/>ORIGINAL':
+                message.append(split_text[i + j])
+                j += 1
+            messages.append(message)
+
+    return messages
+
+
+# helper function to clean messages
+def clean_messages(messages):
+
+    removal_list = ['/>', '<br', 'TRANSLATION', ':', 'SUBSCRIBE']
+
+    for message in messages:
+        for word in message:
+            if word in removal_list:
+                message.remove(word)
+
+    return messages
+
+
+# function to process html file into a list of messages. Each message is itself a list of words. 
+# A message is an original telegram post in English.
+def process(filename):
+
+    # open and read in the html file
+    file = open(filename, 'r')
+    contents = file.read()
+    soup = BeautifulSoup(contents, 'lxml')
+
+    # convert html into text
+    text = soup.text
+    split_text = text.split()
+
+    # retrieve messages from the text
+    messages = retrieve_messages(split_text)
+
+    # clean the messages
+    clean_messages(messages)
+
+    return messages
+
+
+# test
+
+filename = 'sample.html'
+
+messages = process(filename)
 
 print(messages[:5])
