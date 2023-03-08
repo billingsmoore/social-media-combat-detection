6fd4f2b8bc1e5bdbee111b23aca919d3f01174cb
Jacob Moore <billingsmoore@gmail.com>
2023-03-08 14:03:23 -0500

    experimented with visualization

--- visualization.py
+++ visualization.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import altair as alt
 
-filename = 'csv/2023-1_2.csv'
+filename = 'test_csv/2023-1_2.csv'
 
 df = pd.read_csv(filename)
 
-chart = alt.Chart(df).mark_point().encode(
+neg_time = alt.Chart(df).mark_point().encode(
     x=alt.X('datetime', timeUnit='hours'),
     y='negativity',
-    color =alt.Color('oblast', legend=alt.Legend(title="Negative Sentiment in Ukrainian Telegram Messages")),
+    color =alt.Color('oblast', legend=alt.Legend(title="Negative Sentiments")),
     tooltip = 'message',
-).save('negativity.html')
+).save('test_viz/neg_time.html')
+
+neg_oblast = alt.Chart(df).mark_point().encode(
+    x='oblast',
+    y='negativity',
+    color =alt.Color('datetime', timeUnit='hours', legend=alt.Legend(title="Negative Sentiments")),
+    tooltip = 'message',
+).save('test_viz/neg_oblasts.html')
+
