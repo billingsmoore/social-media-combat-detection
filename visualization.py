c6a8a637e26da771bd2f34f3bd36e57571e54ab3
Jacob Moore <billingsmoore@gmail.com>
2023-03-08 13:24:43 -0500

    removed unhelpful visualizations

--- visualization.py
+++ visualization.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import altair as alt
 
-df = pd.read_csv('2023-01-01.csv')
+filename = 'csv/2023-1_2.csv'
+
+df = pd.read_csv(filename)
 
 chart = alt.Chart(df).mark_point().encode(
     x=alt.X('datetime', timeUnit='hours'),
@@ -9,17 +11,3 @@ chart = alt.Chart(df).mark_point().encode(
     color =alt.Color('oblast', legend=alt.Legend(title="Negative Sentiment in Ukrainian Telegram Messages")),
     tooltip = 'message',
 ).save('negativity.html')
-
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X('datetime', timeUnit='hours'),
-    y='positivity',
-    color =alt.Color('oblast', legend=alt.Legend(title="Positive Sentiment in Ukrainian Telegram Messages")),
-    tooltip = 'message',
-).save('positivity.html')
-
-chart = alt.Chart(df).mark_point().encode(
-    x=alt.X('datetime', timeUnit='hours'),
-    y='compound',
-    color =alt.Color('oblast', legend=alt.Legend(title="Compound Sentiment in Ukrainian Telegram Messages")),
-    tooltip = 'message',
-).save('compound.html')
