197390be84f5a16ff7c353d1cf478f44abd3cd31
Jacob Moore <billingsmoore@gmail.com>
2023-03-08 21:55:53 -0500

    updated for reusability

--- url_to_csv.py
+++ url_to_csv.py
@@ -1,20 +1,31 @@
 import get_html
 import html_to_csv
 
-# key info
-year = '2022'
-month = '04'
-date = year + '-' + month
-last_page_num = 20
+def scrape(start, end):
+    for i in range(start, end + 1):
+        # key info
+        year = '2023'
+        month = str(i)
+        if len(month) < 2:
+            month = '0' + month
+        date = year + '-' + month
+        last_page_num = 150
 
-# get the first page
-url = 'https://ukraine.osintukraine.com/' + date + '.html'
-filename = '2023-1'
-filename = get_html.get_html(url, filename)
-html_to_csv.html_to_csv(filename)
+        # get the first page
+        url = 'https://ukraine.osintukraine.com/' + date + '.html'
+        filename = date + '_1'
+        filename = get_html.get_html(url, filename)
+        html_to_csv.html_to_csv(filename)
 
-for i in range(2,last_page_num):
-    url = 'https://ukraine.osintukraine.com/'+ date + '_' + str(i) + '.html'
-    filename = '2023-1_' + str(i)
-    filename = get_html.get_html(url, filename)
-    html_to_csv.html_to_csv(filename)
+        for j in range(2,last_page_num):
+            try:
+                url = 'https://ukraine.osintukraine.com/'+ date + '_' + str(j) + '.html'
+                filename = date + '_' + str(j)
+                filename = get_html.get_html(url, filename)
+                html_to_csv.html_to_csv(filename)
+            except:
+                break
+
+scrape(1, 2)
+
+print('Done!')
