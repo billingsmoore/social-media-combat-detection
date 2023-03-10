b3f0e215fe785d3b65465560c5b8d44e62dc1564
Jacob Moore <billingsmoore@gmail.com>
2023-03-10 17:57:00 -0500

    enrich with battles is working but not on daily_means data, it's not treating that datetime as daily_means['datetime'] for some reason need to get that fixed before I can model

--- frametests.py
+++ frametests.py
@@ -1,20 +1,20 @@
 import pandas as pd
+from enrich_with_battles import enrich_with_battles
 
-uk = pd.read_csv('data/ukrainekyiv.csv')
+uk = pd.read_csv('../sentimental-data/kyiv/ukrainekyiv.csv')
 
 uk = uk.drop(columns=['Unnamed: 0.2', 'Unnamed: 0.1', 'Unnamed: 0', 'message', 'city', 'oblast'])
 
 uk_daily_means = uk.groupby(pd.to_datetime(uk['datetime']).dt.date)[['negativity', 'neutrality', 'positivity', 'compound']].mean()
 
 
-
-ru = pd.read_csv('data/russiakyiv.csv')
+# load in russia
+ru = pd.read_csv('../sentimental-data/kyiv/russiakyiv.csv')
 
 ru = ru.drop(columns=['Unnamed: 0.2', 'Unnamed: 0.1', 'Unnamed: 0', 'message', 'city', 'oblast'])
 
 ru_daily_means = ru.groupby(pd.to_datetime(ru['datetime']).dt.date)[['negativity', 'neutrality', 'positivity', 'compound']].mean()
 
-print(ru_daily_means)
 
 uk_daily_means.rename(columns = {'negativity' : 'uk_negativity',
                                  'neutrality': 'uk_neutrality',
@@ -28,3 +28,4 @@ ru_daily_means.rename(columns = {'negativity' : 'ru_negativity',
 
 daily_means = uk_daily_means.join(ru_daily_means, on='datetime')
 
+print(daily_means.head())
