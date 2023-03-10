19678f0aa25719af466d2d7dacfaabfd77bd4039
Jacob Moore <billingsmoore@gmail.com>
2023-03-10 17:58:17 -0500

    enrich_with_battles is working but not on daily_means data because its not treating the datetime as daily_means['datetime'] for some reason need to fix before I can model

--- enrich_with_battles.py
+++ enrich_with_battles.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('data/ukrainekyiv.csv')
+df = pd.read_csv('../sentimental-data/kyiv/ukrainekyiv.csv')
 
 kyiv_battles = [['24 February 2022 ', '25 February 2022 '],
                 ['24 February 2022 ', '24 February 2022 '],
@@ -19,9 +19,24 @@ for battle in kyiv_battles:
     for date in battle:
         date = pd.to_datetime(date)
 
-def enrich_with_battles(df):
-    global kyiv_battles
-    for index, row in df.iterrows():
-        for battle in kyiv_battles:
-            # TODO
-            #if row['datetime'] 
+def enrich_with_battles(row):
+    kyiv_battles = [['24 February 2022 ', '25 February 2022 '],
+                ['24 February 2022 ', '24 February 2022 '],
+                ['25 February 2022 ', '27 February 2022 '],
+                ['25 February 2022 ', '1 April 2022'],
+                ['26 February 2022', '26 February 2022'], 
+                ['27 February 2022', '31 March 2022'],
+                ['27 February 2022', '28 March 2022'],
+                ['27 February 2022', '25 March 2022'],
+                ['5 March 2022', '21 March 2022'],
+                ['9 March 2022', '1 April 2022 '],
+                ['18 March 2022', '27 March 2022'],
+                ['25 February 2022 ', '31 March 2022']]
+    for battle in kyiv_battles:
+        if row['datetime'] >= battle[0] and row['datetime'] <= battle[1]:
+            return True
+    return False
+
+df['fighting'] = df.apply(enrich_with_battles, axis=1)
+
+print(df.head())
