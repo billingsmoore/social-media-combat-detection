c106a87a2af1a4df8aa20c3d29247b92c218b4eb
Jacob Moore <billingsmoore@gmail.com>
2023-03-12 13:48:10 -0400

    first past at modeling complete not very accurate but better than chance need to try cleaning messages and assigning more accurate scores

---
+++ modeling/modeling.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+# load in data
+df = pd.read_csv('kharkiv_complete_data.csv')
+
+df = df.dropna()
+
+x = df.loc[:,'uk_negativity':'ru_compound']
+
+y = df['fighting']
+
+# split into train and test
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
+
+# normalize data
+ss_train = StandardScaler()
+x_train = ss_train.fit_transform(x_train)
+
+ss_test = StandardScaler()
+x_test = ss_test.fit_transform(x_test)
+
+# initialize models
+models = {}
+
+models['Logistic Regression'] = LogisticRegression()
+models['Support Vector Machines'] = LinearSVC()
+models['Decision Trees'] = DecisionTreeClassifier()
+models['Random Forest'] = RandomForestClassifier()
+models['Naive Bayes'] = GaussianNB()
+models['K-Nearest Neighbor'] = KNeighborsClassifier()
+
+
+# model
+accuracy, precision, recall = {}, {}, {}
+
+for key in models.keys():
+    
+    # fit the classifier
+    models[key].fit(x_train, y_train)
+
+    #make predictions
+    predictions = models[key].predict(x_test)
+
+    #calculate metrics
+    accuracy[key] = accuracy_score(predictions, y_test)
+    precision[key] = precision_score(predictions, y_test)
+    recall[key] = recall_score(predictions, y_test)
+
+# print results
+df_model = pd.DataFrame(index=models.keys(), columns=['Accuracy', 'Precision', 'Recall'])
+df_model['Accuracy'] = accuracy.values()
+df_model['Precision'] = precision.values()
+df_model['Recall'] = recall.values()
+
+print(df_model)
